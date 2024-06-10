4d2c62f4b21258e26e6477945581b4be556bffdf
Cody <c.jmiller17@yahoo.com>
2024-06-10 13:23:15 -0400

    First Commit

---
+++ jeu_petit_prince/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JeuPetitPrinceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jeu_petit_prince'
---
+++ jeu_petit_prince/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    account = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    profile_image = models.ImageField(upload_to="images/")
+
+    butterflies = models.BooleanField(blank=True)
+    elephants = models.IntegerField(blank=True)
+    games = models.TextField(blank=True)
+    fav_color = models.CharField(max_length=100)
+
+    birds_collected = models.IntegerField(blank=True)
+    
+    score_little_prince = models.IntegerField(blank=True)
+    score_king = models.IntegerField(blank=True)
+    score_conceited_man = models.IntegerField(blank=True)
+    score_drunkard = models.IntegerField(blank=True)
+    score_business_man = models.IntegerField(blank=True)
+    score_lamplighter = models.IntegerField(blank=True)
+    score_geographer = models.IntegerField(blank=True)
+    score_earth = models.IntegerField(blank=True)
+
+    total_score = models.IntegerField(blank=True)
+
+    item_1 = models.IntegerField(blank=True)
+    item_2 = models.IntegerField(blank=True)
+    item_3 = models.IntegerField(blank=True)
+
+
+
